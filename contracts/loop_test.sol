acc2bd9d27a9928649e2c1d5999ebe8be2266cfc
ivenine59 <jw1021301@gmail.com>
2024-06-01 16:05:27 +0900

    loop contract

---
+++ contracts/loop_test.sol
@@ -0,0 +1,19 @@
+// contracts/loop_test.sol
+pragma solidity ^0.8.0;
+
+contract loop_test {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        for (uint256 i = 0; i < msg.value; i++) {
+            balances[msg.sender] += 1;
+        }        
+    }
+
+    function withdraw(uint256 _amount) public {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+        balances[msg.sender] -= _amount;
+    }
+}
